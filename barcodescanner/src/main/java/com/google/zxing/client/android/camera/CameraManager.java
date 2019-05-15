9e69e56f70dd85928b6ab0431eb972ffa7c4a223
Chris de-Vos <christopher.devos@ntu.ac.uk>
2019-05-15 10:59:55 +0100

    Update CameraManager.java
    Added changes to getFramingRect() to reintroduce our fix for the incorrect (too small) viewfinder

--- barcodescanner/src/main/java/com/google/zxing/client/android/camera/CameraManager.java
+++ barcodescanner/src/main/java/com/google/zxing/client/android/camera/CameraManager.java
@@ -236,13 +236,16 @@ public final class CameraManager {
         // Called early, before init even finished
         return null;
       }
-
+/*
       int width = findDesiredDimensionInRange(screenResolution.x, MIN_FRAME_WIDTH, MAX_FRAME_WIDTH);
       int height = findDesiredDimensionInRange(screenResolution.y, MIN_FRAME_HEIGHT, MAX_FRAME_HEIGHT);
-
-      int leftOffset = (screenResolution.x - width) / 2;
-      int topOffset = (screenResolution.y - height) / 2;
-      framingRect = new Rect(leftOffset, topOffset, leftOffset + width, topOffset + height);
+*/
+      double width = screenResolution.x * 0.9;
+      double height = screenResolution.y * 0.9;
+      
+      double leftOffset = (screenResolution.x - width) / 2;
+      double topOffset = (screenResolution.y - height) / 2;
+      framingRect = new Rect((int)leftOffset, (int)topOffset, (int)(leftOffset + width), (int)(topOffset + height));
       Log.d(TAG, "Calculated framing rect: " + framingRect);
     }
     return framingRect;
